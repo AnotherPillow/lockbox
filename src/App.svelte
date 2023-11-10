004ecc67bc9b389944f78371f359abf149f299f7
AnotherPillow <Thepilous_@outlook.com>
2023-11-10 22:00:11 +0000

    Change icon

--- src/App.svelte
+++ src/App.svelte
@@ -9,34 +9,10 @@
 <Toasts />
 
 <main class="container">
+
     <NavBar />
-    <!-- <h1>Welcome to Tauri!</h1> 
-
-    <div class="row">
-        <ExampleLogos />
-    </div> 
-
-
-    <p>
-        Click on the Tauri, Vite, and Svelte logos to learn more.
-    </p>
-
-    <div class="row">
-        <Greet />
-    </div>
-    
-    <div class="row">
-        <HashKey />
-    </div>
-
-    <div class="row">
-        <StartUpData />
-    </div> -->
 
     <MainArea />
-        
-  
-
 
 </main>
 
